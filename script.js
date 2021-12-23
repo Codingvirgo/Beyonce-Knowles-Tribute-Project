7b5dbf080e47f3ba2b577f396b4fce915d7a7fd2
Raelyn Brown <msraerae2000@gmail.com>
2021-12-23 15:23:50 -0600

    add prompt window

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ console.log(loveBey);
 
 if(loveBey == `yes`) {
      console.log(`THAT'S WHAT I'M TALKING ABOUT`);
-} else if(loveBey){
-     console.log()
+} else if(loveBey == `no`) {
+     console.log(`Get off this holy page you hating heathen`);
+} else {
+     console.log(`There's only one real answer here...`);
 }
